f1a3a07fa4b34721c9f66d9d0e95d61953d271fc
Naimishtanti <nemish.stackapp@gmail.com>
2023-08-15 18:04:40 +0530

    change: Route Helper class

--- lib/routes/routes_helper.dart
+++ lib/routes/routes_helper.dart
@@ -1,18 +1,19 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:signal/pages/home/home_screen.dart';
 import 'package:signal/pages/intro_page/intro_page.dart';
+import 'package:signal/pages/otp_pages/verify_otp_page.dart';
+import 'package:signal/pages/signin_pages/sign_in_page.dart';
 import 'package:signal/routes/routes_constant.dart';
 
 class RouteHelper {
   static String getSignInPage() => RouteConstant.signInPage;
-
   static String getVerifyOtpPage() => RouteConstant.verifyOtpScreen;
-
   static String getHomeScreen() => RouteConstant.homeScreen;
 
   static List<GetPage> routes = [
     GetPage(name: RouteConstant.homeScreen, page: () => HomeScreen()),
-
     GetPage(name: RouteConstant.initial, page: () => IntroPage()),
+    GetPage(name: RouteConstant.signInPage,page: () => SignInPage(),),
+    GetPage(name: RouteConstant.verifyOtpScreen, page: () => VerifyOtpPage(),)
   ];
 }
